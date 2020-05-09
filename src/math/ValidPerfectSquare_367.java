ac283901a7bf332f867f212a7c9d74ed897e1543
Aleksandr Birin <sza1@ya.ru>
2020-05-09 12:05:14 +0400

    ValidPerfectSquare_367 proper binary search

--- src/math/ValidPerfectSquare_367.java
+++ src/math/ValidPerfectSquare_367.java
@@ -17,6 +17,21 @@ public class ValidPerfectSquare_367 {
     }
 
     public boolean isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPerfectSquare2(int num) {
         long e = num;
         while (e * e > num) {
             e >>= 1;
