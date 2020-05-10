3f6214a6bbf32955af3d56b00d36cb234c21321a
Aleksandr Birin <sza1@ya.ru>
2020-05-10 12:35:19 +0400

    TownJudje 997

--- src/array/TownJudje_997.java
+++ src/array/TownJudje_997.java
@@ -17,16 +17,16 @@ import java.util.Set;
  * <p>
  * If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
  */
-public class TownJudje {
+public class TownJudje_997 {
 
     public static void main(String[] args) {
-        System.out.println(new TownJudje().findJudge(2, new int[][]{{1, 2}}));
-        System.out.println(new TownJudje().findJudge(3, new int[][]{{1, 3}, {2, 3}}));
-        System.out.println(new TownJudje().findJudge(3, new int[][]{{1, 3}, {2, 3}, {3, 1}}));
-        System.out.println(new TownJudje().findJudge(3, new int[][]{{1, 2}, {2, 3}}));
-        System.out.println(new TownJudje().findJudge(4, new int[][]{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}));
-        System.out.println(new TownJudje().findJudge(4, new int[][]{{1, 2}, {1, 3}, {2, 1}, {2, 3}, {1, 4}, {4, 3}, {4, 1}}));
-        System.out.println(new TownJudje().findJudge(11, new int[][]
+        System.out.println(new TownJudje_997().findJudge(2, new int[][]{{1, 2}}));
+        System.out.println(new TownJudje_997().findJudge(3, new int[][]{{1, 3}, {2, 3}}));
+        System.out.println(new TownJudje_997().findJudge(3, new int[][]{{1, 3}, {2, 3}, {3, 1}}));
+        System.out.println(new TownJudje_997().findJudge(3, new int[][]{{1, 2}, {2, 3}}));
+        System.out.println(new TownJudje_997().findJudge(4, new int[][]{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}));
+        System.out.println(new TownJudje_997().findJudge(4, new int[][]{{1, 2}, {1, 3}, {2, 1}, {2, 3}, {1, 4}, {4, 3}, {4, 1}}));
+        System.out.println(new TownJudje_997().findJudge(11, new int[][]
                 {{1, 8}, {1, 3}, {2, 8}, {2, 3}, {4, 8}, {4, 3}, {5, 8}, {5, 3}, {6, 8}, {6, 3}, {7, 8}, {7, 3}, {9, 8}, {9, 3}, {11, 8}, {11, 3}}));
     }
 
@@ -37,8 +37,23 @@ public class TownJudje {
      * trust[i][0] != trust[i][1]
      * 1 <= trust[i][0], trust[i][1] <= N
      */
-
     public int findJudge(int N, int[][] trusts) {
+        int[] memo = new int[N+1];
+
+        for (int[] trust : trusts) {
+            memo[trust[0]]--;
+            memo[trust[1]]++;
+        }
+
+        int k = 0;
+        for (int i = 0; i <= N; i++) {
+            if(memo[i] == N-1)
+                return i;
+        }
+        return -1;
+    }
+
+    public int findJudge2(int N, int[][] trusts) {
         if (trusts.length == 0 && N == 0)
             return -1;
         if (trusts.length == 0 && N == 1)
