7b08d54a1a6c8f3454f6fb49b8f4a086dba1d969
Aleksandr Birin <sza1@ya.ru>
2020-05-31 12:47:44 +0400

    LongestIncreasingSubsequence_300 n^2

---
+++ src/array/LongestIncreasingSubsequence_300.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence_300 {
+
+    public static void main(String[] args) {
+        System.out.println(new LongestIncreasingSubsequence_300().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int result = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
