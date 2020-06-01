d3578dc4ad9dca5e28991a27d77e010ec78fd406
Aleksandr Birin <sza1@ya.ru>
2020-06-01 12:44:40 +0400

    MajorityElement_169

--- src/array/MajorityElement_169.java
+++ src/array/MajorityElement_169.java
@@ -7,14 +7,15 @@ public class MajorityElement_169 {
     }
 
     public int majorityElement(int[] num) {
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
+        int major = num[0], count = 1;
+        for (int i = 1; i < num.length; i++) {
+            if (count == 0) {
                 count++;
-                major=num[i];
-            }else if(major==num[i]){
+                major = num[i];
+            } else if (major == num[i]) {
                 count++;
-            }else count--;
+            } else
+                count--;
 
         }
         return major;
