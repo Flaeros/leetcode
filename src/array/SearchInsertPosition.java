75a48bc9da1b79ac5f06742512bdf8cd08bd9fc4
Aleksandr Birin <sza1@ya.ru>
2020-06-10 19:02:56 +0400

    SearchInsertPosition

---
+++ src/array/SearchInsertPosition.java
@@ -0,0 +1,32 @@
+package array;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        System.out.println(new SearchInsertPosition().searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(new SearchInsertPosition().searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(new SearchInsertPosition().searchInsert(new int[]{1, 3, 5, 6}, 7));
+        System.out.println(new SearchInsertPosition().searchInsert(new int[]{1, 3, 5, 6}, 0));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0)
+            return 0;
+
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
