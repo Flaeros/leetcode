4c2a0ea114381ed8651eea9da5218b8d2a274e98
Aleksandr Birin <sza1@ya.ru>
2020-06-27 12:59:35 +0400

    PerfectSquares_279 for contest

--- src/dp/PerfectSquares_279.java
+++ src/dp/PerfectSquares_279.java
@@ -1,6 +1,8 @@
 package dp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PerfectSquares_279 {
 
@@ -17,6 +19,32 @@ public class PerfectSquares_279 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+
+    public int numSquares2(int n) {
+        List<Integer> squares = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n == i * i)
+                return 1;
+            squares.add(i * i);
+        }
+
+        int[] dp = new int[n+1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = i;
+        }
+        for (int sq : squares) {
+            for (int j = 1; j <= n; j++) {
+                if (sq == j) {
+                    dp[j] = 1;
+                } else if (j >= sq) {
+                    dp[j] = Math.min(dp[j],  dp[j - sq] + 1);
+                }
             }
         }
 
