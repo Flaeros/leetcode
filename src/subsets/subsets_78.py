91707ef7b77073e0523a8d8780a13a539ec738be
Aleksandr Birin <flaeross@yandex-team.ru>
2022-02-13 16:59:49 +0300

    subsets

---
+++ src/subsets/subsets_78.py
@@ -0,0 +1,32 @@
+# exclude/include
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
+        self.find_subsets_rec(nums, subsets, [], 0)
+        subsets.append([])
+        return subsets
+
+
+    def find_subsets_rec(self, nums, subsets, subset, index):
+        if len(nums) <= index:
+            return
+
+        # exclude
+        self.find_subsets_rec(nums, subsets, subset, index + 1)
+
+        #include
+        subset.append(nums[index])
+        subsets.append(subset.copy())
+        self.find_subsets_rec(nums, subsets, subset, index + 1)
+        subset.pop()
+
+
+def main():
+    print("Here is the list of subsets: " + str(Solution().subsets([1, 3])))
+    print("Here is the list of subsets: " + str(Solution().subsets([1, 5, 3])))
+
+
+main()
