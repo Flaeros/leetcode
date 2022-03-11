256961c7444b7538f4c073cd002b88c1eae80a31
Aleksandr Birin <flaeross@yandex-team.ru>
2022-03-11 11:25:08 +0300

    926, monotone increasing

---
+++ src/dp/monotone_increasing_926.py
@@ -0,0 +1,22 @@
+# P[i+1] = A[0] + ... + A[i]
+# for x in range(len(s))
+# res[x] = P[x] + (N-x -(P[n] - P[x]))
+
+class Solution(object):
+    def minFlipsMonoIncr(self, s):
+        n = len(s)
+        p = [int(s[0])]
+
+        for char in s:
+            p.append(int(char) + p[-1])
+
+        result = n
+        for i in range(len(p)):
+            result = min(result, p[i] + n - i - (p[-1] - p[i]))
+
+        return result
+
+
+print(Solution().minFlipsMonoIncr("11011"))
+print(Solution().minFlipsMonoIncr("00011000"))
+print(Solution().minFlipsMonoIncr("00110"))
