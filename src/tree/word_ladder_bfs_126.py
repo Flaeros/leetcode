71890fd290ea92eb8040317f66da35a1ef84e8ad
Aleksandr Birin <flaeross@yandex-team.ru>
2022-04-12 11:31:04 +0300

    126, no memory

--- src/tree/word_ladder_bfs_126.py
+++ src/tree/word_ladder_bfs_126.py
@@ -13,11 +13,12 @@ def oneCharDifference(prev, next):
 
 
 class Solution:
-    def ladderLength(self, start, end, words):
+    def ladderLengthAdj(self, start, end, words):
+        if end not in words:
+            return False
 
         adjacency = defaultdict(set)
 
-        words.append(end)
         words.append(start)
 
         for prev in words:
@@ -47,6 +48,21 @@ class Solution:
 
         return 0
 
+    def ladderLength(self, start, end, words):
+        words = set(words)
+        queue = deque([[start, 1]])
+        while queue:
+            word, length = queue.popleft()
+            if word == end:
+                return length
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i + 1:]
+                    if next_word in words:
+                        words.remove(next_word)
+                        queue.append([next_word, length + 1])
+        return 0
+
 
 print(Solution().ladderLength("hit",
                               "cog",
@@ -55,5 +71,13 @@ print(Solution().ladderLength("hit",
 print(Solution().ladderLength(start="hit",
                               end="cog",
                               words=["hix", "dog", "log", "hot", "dot", "lot"]))
+
+print(Solution().ladderLengthAdj("hit",
+                                 "cog",
+                                 ["hot", "dot", "dog", "lot", "log"]))
+
+print(Solution().ladderLengthAdj(start="hit",
+                                 end="cog",
+                                 words=["hix", "dog", "log", "hot", "dot", "lot"]))
 
 # "hit" -> "hot" -> "dot" -> "dog" -> cog"
